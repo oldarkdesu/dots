6bf53b68be4941578da60f9543d6e98b8e7ea3cf
OldarkDesu <oldarkdesu@gmail.com>
2025-09-21 05:27:40 -0500

    testing a thingy

--- bash/.config/bash/functions.sh
+++ bash/.config/bash/functions.sh
@@ -29,3 +29,26 @@ open() {
 		return 1
 	fi
 }
+
+encrypt () {
+	if [ "$1" = "s" -o "$1" = "sym" -o "$1" = "symmetric" ] ; then
+		echo "Using symmetric encryption..."
+		if [ -d "$2" ] ; then 
+			tar cf "${2}.tar" $2 && gpg -c "${2}.tar" || echo -e "\e[31mERROR\e[m"
+			return $?
+		elif [ -f "$2" ] ; then
+			gpg -c "${2}" || echo -e "\e[31mERROR\e[m"
+			return $?
+		else
+			echo -e "\e[31mERROR: \e[m file not supported"
+			return 1
+		fi
+	elif [ "$1" = "a" -o "$1" = "asym" -o "$1" = "asymmetric" ]; then
+		echo "Using asymmetric encryption..."
+		echo -e "\e[33mWARNING: \e[m not implemented yet..."
+		return 1
+	else
+		echo -e "\e[31mERROR: \e[m action not supported"
+		return 1
+	fi
+}

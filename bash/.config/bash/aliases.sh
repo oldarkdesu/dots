cd5d095b7b4831a94c6cd39d502add532f75a499
OldarkDesu <oldarkdesu@gmail.com>
2025-09-23 01:06:21 -0500

    i'm an idiot

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -44,7 +44,7 @@ alias pn='pnpm'
 alias rm="echo Are you sure? Use '\rm', or the full path i.e. '/bin/rm'"
 
 # Debian/Ubuntu
-alias aptup="pls bash -c 'apt update -y && apt upgrade -y && echo -e \"\n\[\e[1;92m\][APT UPGRADE FINISHED]\[\e[0m\]\"'"
+alias aptup="pls bash -c 'apt update -y && apt upgrade -y && echo -e \"\n\e[1;92m[APT UPGRADE FINISHED]\e[0m\"'"
 
 alias codefp='flatpak run com.visualstudio.code'
 alias showpath="echo $PATH | sed 's/:/\n/g'"

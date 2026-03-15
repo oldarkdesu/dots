db145a70ca09b2b717677d40422f8d46c887782c
OldarkDesu <oldarkdesu@gmail.com>
2026-03-15 17:19:26 -0500

    asdf

--- pkgs/scripts/.local/bin/scripts/figlet_test.sh
+++ pkgs/scripts/.local/bin/scripts/figlet_test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # this works at least on arch linux
-for f in /usr/share/figlet/* ; do _font=$(basename $f) ; echo $_font: ; figlet -d /usr/share/figlet -f $_font ${1:-whatever} ${@:2} ; done ; unset _font
+for f in /usr/share/figlet/*.tlf /usr/share/figlet/fonts/*.flf ; do
+	echo $f: ; figlet -t -f $f ${1:-whatever} ${@:2}
+done

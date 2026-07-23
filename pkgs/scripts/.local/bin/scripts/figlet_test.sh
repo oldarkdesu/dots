8f8fe587044bcf0fc833cfac939ae910b2768349
OldarkDesu <oldarkdesu@gmail.com>
2026-07-23 12:36:55 -0500

    lazyly committing all changes...

--- pkgs/scripts/.local/bin/scripts/figlet_test.sh
+++ pkgs/scripts/.local/bin/scripts/figlet_test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-# this works at least on arch linux (after installing the `figlet` pacage ofc)
+
+# make sure figlet is installed/available
+if ! command -v figlet 2>&1 >/dev/null ; then
+	echo 'Command `figlet` is not available' >&2
+	exit 1
+fi
+
 for f in /usr/share/figlet/*.tlf /usr/share/figlet/fonts/*.flf ; do
 	echo $f: ; figlet -t -f $f ${1:-whatever} ${@:2}
 done

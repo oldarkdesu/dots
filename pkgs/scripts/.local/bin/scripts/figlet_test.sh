481cadefe06a482a3679ff801ca8e482c884eeff
OldarkDesu <oldarkdesu@gmail.com>
2026-07-23 12:33:37 -0500

    lazyly committing all changes...

--- pkgs/scripts/.local/bin/scripts/figlet_test.sh
+++ pkgs/scripts/.local/bin/scripts/figlet_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# this works at least on arch linux
+# this works at least on arch linux (after installing the `figlet` pacage ofc)
 for f in /usr/share/figlet/*.tlf /usr/share/figlet/fonts/*.flf ; do
 	echo $f: ; figlet -t -f $f ${1:-whatever} ${@:2}
 done

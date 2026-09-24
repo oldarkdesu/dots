07c4504e3c9be8bfbb7f29451d3d1426ba21b54e
OldarkDesu <oldarkdesu@gmail.com>
2026-09-23 23:47:53 -0500

    lazyly committing all changes...

--- .misc/hwdb.d/update_hwdb_config.sh
+++ .misc/hwdb.d/update_hwdb_config.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env sh
 
+files="tartarus-v2.hwdb trackball.hwdb"
+dest="/etc/udev/hwdb.d"
+
 if [ "$(whoami)" != 'root' ] ; then
 	echo Not running on a priviliged shell. Exiting. >&2
-	exit 1
+	# exit 1
 fi
 
 # Note to self: Don't touch the /usr/lib directory, apparently is for system files only. Configuration should go on /etc
-for f in tartarus-v2 trackball ; do
-	cp /etc/udev/hwdb.d/${f}.hwdb ./${f}.hwdb.bak
-	cp ./${f}.hwdb /etc/udev/hwdb.d/${f}.hwdb
+for f in $files ; do
+	if [ -f ./${f}.hwdb ] ; then
+		echo "File \`$f\` not found in this directory" >&2
+		continue
+	fi
+	echo "Copying $f into $dest/..."
+	cp /etc/udev/hwdb.d/${f} ./${f}.bak
+	cp ./${f} /etc/udev/hwdb.d/${f}
+
 done
 
-systemd-hwdb update
-udevadm trigger
+echo "Applying settings..."
+systemd-hwdb update && udevadm trigger && echo Done
